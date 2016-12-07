670b45b5b50c89d51b46f59e2469c278dadb719e
wandecleya <wandecleya@gmail.com>
2016-12-07 00:06:54 -0200

    adds nowadays rules

--- calculadora.js
+++ calculadora.js
@@ -1,14 +1,22 @@
 
+function foo(gender, age) {
+  var gender = gender;
+  var age = age;
+
+  if((gender == "F") && (age <=45) || (gender == "M") && (age <=50)){
+    return "newRules";
+  }
+};
 
 var calculadora = function () {
-  var gender = "f";
+  var gender = "M";
   var age = 45;
   var contribution = 2;
+  var rules = foo(gender, age);
 
-  if((gender == "f") && (age <=45)){
+  if (rules == "newRules") {
     var age_time = 65 - age;
     var contribution_time = 25 - contribution;
-    alert(Math.max(age_time, contribution_time));
+    var time_left = Math.max(age_time, contribution_time));
   }
-
-}
+};
